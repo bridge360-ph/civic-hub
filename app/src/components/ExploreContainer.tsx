f091b03bb803e9a4e99ea27a62991a3139d0b1b4
teamie <79058304+tea-scripts@users.noreply.github.com>
2024-02-09 00:40:45 +0000

    chore: bootstrap app using ionic

---
+++ app/src/components/ExploreContainer.tsx
@@ -0,0 +1,13 @@
+import './ExploreContainer.css';
+
+interface ContainerProps { }
+
+const ExploreContainer: React.FC<ContainerProps> = () => {
+  return (
+    <div id="container">
+      <strong>Hello World</strong>
+    </div>
+  );
+};
+
+export default ExploreContainer;
